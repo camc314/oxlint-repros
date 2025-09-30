62d7c99bccd17a1ec64941f34c206100c654aa39
Cameron Clark <cameron.clark@hey.com>
2025-09-30 16:39:51 +0100

    update repro

--- issue-report-unused-imports-13706/src/index.ts
+++ issue-report-unused-imports-13706/src/index.ts
@@ -1,1 +1,20 @@
 import { foo } from 'bar';
+// `bar` is unused, but reported
+
+const { foo } = bar;
+// `foo` is unused, but not reported
+
+// `unusedFunction` is unused, but reported
+// it's safe to remove
+function unusedFunction(a, b) {
+    // a,b are both unused, but not reported
+}
+
+const x = (() => {
+    throw new Error('oops');
+})();
+// `x` is unused, and reported
+
+try {
+} catch (e) {}
+// `e` is unused, but reported
