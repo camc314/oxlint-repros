cf94ad6ba3ff98655c575fa2a232e84e31d46a06
Cameron Clark <cameron.clark@hey.com>
2025-09-30 16:38:34 +0100

    add repro for issue-lodash-import-scope

---
+++ issue-lodash-import-scope/src/fail.ts
@@ -0,0 +1,7 @@
+/*eslint lodash/import-scope: [2, "method"]*/
+
+import _ from 'lodash'; //Import individual methods from the Lodash module.
+
+import { map as map2 } from 'lodash'; //Import individual methods from the Lodash module.
+
+import map3 from 'lodash.map'; //Import individual methods from the Lodash module.
