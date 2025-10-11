094ef6243a0c92dfba077c91564cfb95a8979fee
Cameron Clark <cameron.clark@hey.com>
2025-10-11 19:37:03 +0100

    update deps in rules-of-hooks-issue

--- issue-lin-rules-of-hooks/eslint.config.mjs
+++ issue-lin-rules-of-hooks/eslint.config.mjs
@@ -4,4 +4,9 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import reacthookseslint from 'eslint-plugin-react-hooks';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, reacthookseslint.configs['recommended-latest']);
+export default tseslint.config(
+    //
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    reacthookseslint.configs.flat.recommended,
+);
