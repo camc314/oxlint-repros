cfb8253a35bae9830243f5db835e915cccc0fad8
Cameron Clark <cameron.clark@hey.com>
2025-11-27 08:01:14 +0000

    repro issue

---
+++ issue-type-aware-not-resolving-react/dist-tsc/index.js
@@ -0,0 +1,2 @@
+import * as React from 'react';
+React.createElement('div', null, 'Hello, world!');
---
+++ issue-type-aware-not-resolving-react/src/index.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+
+React.createElement('div', null, 'Hello, world!');
